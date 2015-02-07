765f4e05d1be26cefaeb7bc3679c08a3d9129cb8
Alex Ward <alexwarddev1230@gmail.com>
2015-02-06 16:02:01 -0800

    App Upload

---
+++ src/com/talented/local/Login.java
@@ -0,0 +1,178 @@
+package com.talented.local;
+
+import android.app.Activity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends Activity implements OnClickListener {
+
+	
+	// git check
+	// bla bla
+	private EditText user, pass;
+	private Button mSubmit, mRegister;
+
+	// Progress Dialog
+	private ProgressDialog pDialog;
+
+	// JSON parser class
+	JSONParser jsonParser = new JSONParser();
+
+	// testing on Emulator:
+	private static final String LOGIN_URL = "http://lordwebmaster.com/ProjectR/android_api/login.php";
+
+	// JSON element ids from repsonse of php script:
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		 SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(Login.this);
+         String already_login = sp.getString("username", "");
+         
+         /*
+         if ( !already_login.equals(""))
+         {
+        	Intent i = new Intent(this, MainMenu.class);
+ 			startActivity(i);
+ 			finish();
+         }*/
+		
+		setContentView(R.layout.login);
+
+		// setup input fields
+		user = (EditText) findViewById(R.id.username);
+		pass = (EditText) findViewById(R.id.password);
+
+		// setup buttons
+		mSubmit = (Button) findViewById(R.id.login);
+		mRegister = (Button) findViewById(R.id.register);
+
+		// register listeners
+		mSubmit.setOnClickListener(this);
+		mRegister.setOnClickListener(this);
+	}
+	
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.login:
+			new AttemptLogin().execute();
+			
+			break;
+		case R.id.register:
+			Intent i = new Intent(this, Register.class);
+			startActivity(i);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	
+	
+	class AttemptLogin extends AsyncTask<String, String, String> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(Login.this);
+			pDialog.setMessage("Attempting login...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			// Check for success tag
+			int success = 0;
+			String username = user.getText().toString();
+			String password = pass.getText().toString();
+			try {
+				// Building Parameters
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("username", username));
+				params.add(new BasicNameValuePair("password", password));
+
+				Log.d("request!", "starting");
+				// getting product details by making HTTP request
+				JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST",
+						params);
+
+				// check your log for json response
+				Log.d("Login attempt", json.toString());
+
+				// json success tag
+				success = json.getInt(TAG_SUCCESS);
+				if (success == 1) {
+					Log.d("Login Successful!", json.toString());
+					// save user data
+					SharedPreferences sp = PreferenceManager
+							.getDefaultSharedPreferences(Login.this);
+					Editor edit = sp.edit();
+					edit.putString("username", username);
+					edit.commit();
+					
+					//Intent i = new Intent(Login.this, MainMenu.class);
+					//finish();
+					//startActivity(i);
+					
+					return json.getString(TAG_MESSAGE);
+				} else {
+					Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+					return json.getString(TAG_MESSAGE);
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			catch (Exception e){
+				return "Failed, check your network connection";
+			}
+			
+
+			return null;
+
+		}
+
+		protected void onPostExecute(String file_url) {
+			// dismiss the dialog once product deleted
+			pDialog.dismiss();
+			if (file_url != null && !file_url.equals("Failed, check your network connection")) {
+				Toast.makeText(Login.this, file_url, Toast.LENGTH_LONG).show();
+			}
+			else if (file_url.equals("Failed, check your network connection"))
+			{
+				Toast.makeText(Login.this, file_url, 10).show();
+			}
+
+		}
+
+	}
+
+}
---
+++ src/com/talented/local/Register.java
@@ -0,0 +1,172 @@
+package com.talented.local;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends Activity implements OnClickListener{
+	
+	private EditText user, pass, emailTE;
+	private Button  mRegister;
+	
+	 // Progress Dialog
+    private ProgressDialog pDialog;
+ 
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+    
+    //php register script
+    
+    //localhost :  
+    //testing on your device
+    //put your local ip instead,  on windows, run CMD > ipconfig
+    //or in mac's terminal type ifconfig and look for the ip under en0 or en1
+   // private static final String REGISTER_URL = "http://xxx.xxx.x.x:1234/webservice/register.php";
+    
+    //testing on Emulator:
+    private static final String REGISTER_URL = "http://lordwebmaster.com/ProjectR/android_api/register.php";
+    
+  //testing from a real server:
+    //private static final String REGISTER_URL = "http://www.mybringback.com/webservice/register.php";
+    
+    //ids
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+		
+		user = (EditText)findViewById(R.id.username);
+		pass = (EditText)findViewById(R.id.password);
+		emailTE = (EditText)findViewById(R.id.emailTE);
+		
+		/*
+		zip = (EditText)findViewById(R.id.etZip);
+		state = (EditText)findViewById(R.id.etState);
+		city = (EditText)findViewById(R.id.etCity);
+		
+		address = (EditText)findViewById(R.id.etAddress);
+		busName = (EditText)findViewById(R.id.etBus);
+		*/
+
+		mRegister = (Button)findViewById(R.id.register);
+		mRegister.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+				new CreateUser().execute();
+		
+	}
+	
+	class CreateUser extends AsyncTask<String, String, String> {
+
+		
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Register.this);
+            pDialog.setMessage("Creating User...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+		
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			 // Check for success tag
+            int success;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            String email = emailTE.getText().toString();
+            
+            /*
+            String zipS = zip.getText().toString();
+            String stateS = state.getText().toString();
+            String cityS = city.getText().toString();
+            
+            String addressS = address.getText().toString();
+            String busNameS = busName.getText().toString();
+            */
+            
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+                params.add(new BasicNameValuePair("email", email));
+                
+                
+                /*
+                params.add(new BasicNameValuePair("zip", zipS));
+                params.add(new BasicNameValuePair("state", stateS));
+                params.add(new BasicNameValuePair("city", cityS));
+                
+                params.add(new BasicNameValuePair("address", addressS));
+                params.add(new BasicNameValuePair("busName", busNameS));
+                */
+ 
+                Log.d("request!", "starting");
+                
+                //Posting user data to script 
+                JSONObject json = jsonParser.makeHttpRequest(
+                       REGISTER_URL, "POST", params);
+ 
+                // full json response
+                Log.d("Registering attempt", json.toString());
+ 
+                // json success element
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                	Log.d("User Created!", json.toString());              	
+                	finish();
+                	return json.getString(TAG_MESSAGE);
+                }else{
+                	Log.d("Registering Failure!", json.getString(TAG_MESSAGE));
+                	return json.getString(TAG_MESSAGE);
+                	
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e){
+				return "Failed, check your network connection";
+			}
+ 
+            return null;
+			
+		}
+		
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+            	Toast.makeText(Register.this, file_url, Toast.LENGTH_LONG).show();
+            }
+ 
+        }
+		
+	}
+		 
+
+}
